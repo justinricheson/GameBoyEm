a97aa7f6bf9a6049a0d9db970903a52de5ce6d82
justinricheson <justinricheson@icloud.com>
2016-02-20 18:36:04 -0600

    Add some notes, implement first op

--- GameBoyEm/GameBoyEm/Cpu.cs
+++ GameBoyEm/GameBoyEm/Cpu.cs
@@ -1,35 +1,49 @@
-﻿namespace GameBoyEm
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameBoyEm
 {
     /// <summary>
-    /// Emulates a modified Zilog Z80, 8-bit processor used in the original GameBoy
+    /// Emulates the modified Zilog Z80 (aka Sharp LR35902),
+    /// 4.194304MHz, 8-bit processor used in the original GameBoy.
+    /// Notes: IO is memory mapped. Memory is byte addressable with
+    /// 16 bit addresses (64KB total memory).
     /// </summary>
-    public class Cpu : ICpu
+    public class Cpu
     {
         // Registers
-        public byte A { get; set; }
-        public byte B { get; set; }
-        public byte C { get; set; }
-        public byte D { get; set; }
-        public byte E { get; set; }
-        public byte H { get; set; }
-        public byte L { get; set; }
-        public byte F { get; set; } // Flags
-        public byte SP { get; set; } // Stack pointer
-        public ushort PC { get; set; } // Program counter
+        public byte A { get; private set; } // Accumulator
+        public byte B { get; private set; } // General Purpose
+        public byte C { get; private set; } // ..
+        public byte D { get; private set; }
+        public byte E { get; private set; }
+        public byte H { get; private set; }
+        public byte L { get; private set; }
+        public byte F { get; private set; } // Flags (bits 0-3: unused, 4: carry, 5: half-carry, 6: subtract, 7: zero)
+        public byte SP { get; private set; } // Stack pointer
+        public ushort PC { get; private set; } // Program counter
+        public bool IME { get; private set; } // Interrupt master enable
 
         // Timers (last instruction)
-        public ulong M { get; set; }
-        public ulong T { get; set; }
+        public ulong M { get; private set; }
+        public ulong T { get; private set; }
 
         // Cumulative instruction timers
-        public ulong TotalM { get; set; }
-        public ulong TotalT { get; set; }
+        public ulong TotalM { get; private set; }
+        public ulong TotalT { get; private set; }
 
         private IMmu _mmu;
+        private List<Action> _ops;
 
         public Cpu(IMmu mmu)
         {
             _mmu = mmu;
+            _ops = new List<Action>
+            {
+                NOP,
+                LDBCNN,
+                LDBCA
+            };
         }
 
         public void Run()
@@ -38,8 +52,7 @@
             while (true)
             {
                 var op = _mmu.ReadByte(PC++); // Fetch
-                var decoded = OpDecoder.Decode(op); // Decode
-                decoded(this); // Execute
+                _ops[op](); // Decode, Execute
 
                 TotalM += M;
                 TotalT += T;
@@ -51,6 +64,15 @@
             A = B = C = D = E = H = L = F = SP = 0;
             M = T = TotalM = TotalT = 0;
             PC = 0;
+            IME = false;
         }
+
+        private void NOP() { M = 1; T = 4; }
+
+        // 8-bit Loads
+        private void LDBCA() { _mmu.WriteByte((ushort)((B << 8) + C), A); }
+
+        // 16-bit Loads
+        private void LDBCNN() { }
     }
 }
