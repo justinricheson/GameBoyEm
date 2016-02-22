c4d3cd942d9a88343dc2c7d7a271cc40496df0f3
justinricheson <justinricheson@icloud.com>
2016-02-21 22:32:08 -0600

    One more test

--- GameBoyEm/GameBoyEm.Tests/CpuTests.cs
+++ GameBoyEm/GameBoyEm.Tests/CpuTests.cs
@@ -33,5 +33,19 @@ namespace GameBoyEm.Tests
             // Assert
             Assert.AreEqual(256, cpu.BC);
         }
+
+        [TestMethod]
+        public void INCBC_BCIs65535_ShouldOverflowBC()
+        {
+            // Arrange
+            var mmu = new StubIMmu { ReadByteUInt16 = a => 3 };
+            var cpu = new Cpu(mmu, b: 255, c: 255);
+
+            // Act
+            cpu.Run();
+
+            // Assert
+            Assert.AreEqual(0, cpu.BC);
+        }
     }
 }
